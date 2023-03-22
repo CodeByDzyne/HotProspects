fb59132e39e410647cbe3dc3b3654aeea24ea875
CodeByDzyne <wdenley@dzynesolutions.com>
2023-03-22 14:37:34 +1100

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Wealthy Waz on 22/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
